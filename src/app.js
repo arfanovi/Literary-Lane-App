e63405c0453d682062649ad6b4076db2e7401dab
arfanovi <arfanovi.gub1@gmail.com>
2024-07-06 11:36:50 +0600

    Dynamic fetching data Static with  Modal apply

--- src/app.js
+++ src/app.js
@@ -29,3 +29,34 @@ renderButton() {
 
 }
 }
+
+
+
+class CategoryModel {
+    static show(category) {
+        const modal = document.getElementById('modal');
+        document.getElementById('modal-title').textContent = `${category.name} Books`;
+        const modalBooks = document.getElementById('modal-books');
+
+        modalBooks.innerHTML = category.books.map(book => 
+        `
+        <div class="bg-white p-4 rounded shadow-md text-center">
+        <img src="${book.image}" alt="${book.name}" class="w-full h-32 object-cover m-2"/>
+        <h3 class="text-xl font-bold"> ${book.name}</h3>
+        <p class="text-gray-700 ">${book.author} </p>
+        <a href="${book.url}" target="_blank" class="text-blue-500">Read More</a>
+        
+        </div>
+            
+            
+        `).join('');
+
+        modal.classList.remove("hidden");
+        document.getElementById('close-modal').onclick = CategoryModel.close;
+
+    }
+
+    static close() {
+        document.getElementById('modal').classList.add('hidden')
+    }
+}
